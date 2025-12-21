5151c2d9738f721cde1bb909a8b97bfb5a8d39e4
hey-Gana <ganasubs98@gmail.com>
2025-12-20 19:21:03 -0500

    Configs to deploy in Github Pages

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function Intro() {
     return (
         <div>
-            <h1>Split-A-Bill(Splitable)</h1>
+            <h1 style={{ textAlign: "center" }}>Split-A-Bill</h1>
             <p>Welcome to Split-A-Bill : an app that enable you to split expenses and pay only for what you consumed!</p>
-            <h4>Any bill is splitable!</h4>
+            <h4>Any bill is Split-A-Bill!</h4>
         </div>
     )
 }
